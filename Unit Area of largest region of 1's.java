f68371005a807ad742bcfbcc41f0237647210759
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2020-09-18 03:20:57 +0530

    Update Unit Area of largest region of 1's.java

--- Unit Area of largest region of 1's.java
+++ Unit Area of largest region of 1's.java
@@ -37,3 +37,38 @@ class Area
 }
 
 // BFS
+class Area 
+{
+    static int findMaxArea(int N, int M, int A[][]) 
+    {
+        Queue<Integer> q= new LinkedList<>();
+        boolean[][] visited= new boolean[N][M];
+        int ret=0;
+        
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<M;j++)
+            {
+                if(A[i][j]== 1 && !visited[i][j])
+                { q.add(i); q.add(j); visited[i][j]= true; }
+                
+            int ans=0;        
+            while(!q.isEmpty())
+            {
+                int row= q.poll();
+                int col= q.poll();
+                ans++;
+                
+                for(int ii= Math.max(0, row-1); ii< Math.min(N, row+2); ii++)
+                {
+                    for(int jj= Math.max(0, col-1); jj< Math.min(M, col+2); jj++)
+                        if(!visited[ii][jj] && A[ii][jj]== 1)
+                            { q.add(ii); q.add(jj); visited[ii][jj]= true;}
+                }
+            }
+            
+            ret= Math.max(ret, ans);
+            }
+        }
+        return ret;
+    }
